def1c40f3730c29fb26a2734a272d233f6c0fa8e
Bárbara Daniela Malik <61635416+barbar-a21904946@users.noreply.github.com>
2020-09-25 11:26:37 +0100

    Initial commit

---
+++ Main.java
@@ -0,0 +1,75 @@
+import java.util.Arrays;
+
+public class Main {
+
+    static long soma(int num1, int num2) {
+
+        return (long) num1 + num2;
+    }
+
+    static long soma(int num1, int num2, int num3) {
+        return (long) num1 + num2 + num3;
+    }
+
+    static String concatena(int num1, int num2) {
+        String concatena = "" + num1 + " " + num2;
+        return concatena;
+    }
+
+    static int[] criaArray() {
+        int[] array = new int[5];
+        return array;
+    }
+
+    static void preencheArray(int[] numeros) {
+        int array[] = new int[4];
+        array[0] = 4;
+        array[1] = 9;
+        array[2] = 9;
+        array[3] = 7;
+
+
+    }
+
+    static String converteParaString(int[] numeros) {
+
+        return Arrays.toString(numeros);
+    }
+
+    static long soma(int[] numeros) {
+        if (numeros == null) {
+            return 0;
+        } else {
+            int soma = 0;
+            for (int i = 0; i < numeros.length; i++) {
+                soma += numeros[i];
+            }
+            return soma;
+        }
+
+    }
+
+    static long maior(int[] numeros) {
+
+
+        if (numeros == null) {
+            return 0;
+        } else {
+            int maior = Integer.MIN_VALUE;
+            int indice1 = -1;
+
+            for (int i = 0; i < numeros.length; i++) {
+                if (numeros[i] > maior) {
+                    maior = numeros[i];
+                    indice1 = i;
+
+                }
+            }
+            return maior;
+        }
+
+
+    }
+
+
+}
